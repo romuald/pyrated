ca5348895a32c864a7e24aed9044d9453d2bf567
Romuald Brunet <romuald@chivil.com>
2017-09-12 09:31:33 +0200

    Update setup to use py.test

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
 import sys
+import shlex
 
-# from distutils.core import setup, Extension
 from setuptools import setup, find_packages, Extension
-
+from setuptools.command.test import test as TestCommand
 
 if sys.version_info.major < 3:
     raise RuntimeError('This module only supports python3')
 
 rentry = Extension('pyrated.rentry',
                     sources = ['src/rentry.c'])
+
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ''
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
 
 setup(
     name = 'pyrated',
@@ -21,5 +38,8 @@ setup(
     ext_modules = [rentry],
     entry_points = {
         'console_scripts': ['pyrated=pyrated.server:main'],
-    }
+    },
+    test_suite='pyrated.tests',
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest},
 )
