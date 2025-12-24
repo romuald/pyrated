cbf84d20f65c9fe46e2c15f045ae5a379cc1b4da
Romuald Brunet <romuald@gandi.net>
2025-12-24 10:50:30 +0100

    Fix performance test script

--- utils/performance.py
+++ utils/performance.py
@@ -6,7 +6,7 @@ import sys
 from itertools import cycle
 
 from time import time, sleep
-from pyrated.ratelimit import RatelimitList
+from pyrated.ratelimit import Ratelimit
 
 
 C = 3000000
@@ -14,7 +14,7 @@ N = 10
 D = 1000
 K = '129.28.31.240'
 
-r = RatelimitList(N, D)
+r = Ratelimit(N, D)
 s = time()
 for i in range(C):
     r.hit(K)
@@ -22,7 +22,7 @@ d = time() - s
 print('Single key (%d chars), %d entries, %d loops: %.3fs (%d/s)' % (len(K), N, C, d, C / d)) 
 
 N = 2000
-r = RatelimitList(N, D)
+r = Ratelimit(N, D)
 s = time()
 for i in range(C):
     r.hit(K)
@@ -32,7 +32,7 @@ print('Single key (%d chars), %d entries, %d loops: %.3fs (%d/s)' % (len(K), N, 
 KEYS = ('10.0.1.20', '15,2.8.4', '192.168.0.4', '9.9.4.1', '244.200.8.1')
 IKEYS = KEYS * int(C/len(KEYS))
 N = 10
-r = RatelimitList(N, D)
+r = Ratelimit(N, D)
 s = time()
 for k in IKEYS:
     r.hit(k)
@@ -40,7 +40,7 @@ d = time() - s
 print('%d keys, %d entries, %d loops: %.3fs (%d/s)' % (len(KEYS), N, len(IKEYS), d, len(IKEYS) / d)) 
 
 N = 5000
-r = RatelimitList(N, D)
+r = Ratelimit(N, D)
 s = time()
 for k in IKEYS:
     r.hit(k)
@@ -50,7 +50,7 @@ print('%d keys, %d entries, %d loops: %.3fs (%d/s)' % (len(KEYS), N, len(IKEYS),
 KEYS = tuple('1.2.3.%d' % i for i in range(50000))
 IKEYS = KEYS * int(C/len(KEYS))
 N = 10
-r = RatelimitList(N, D)
+r = Ratelimit(N, D)
 s = time()
 for k in IKEYS:
     r.hit(k)
@@ -58,7 +58,7 @@ d = time() - s
 print('%d keys, %d entries, %d loops: %.3fs (%d/s)' % (len(KEYS), N, len(IKEYS), d, len(IKEYS) / d)) 
 
 N = 5000
-r = RatelimitList(N, D)
+r = Ratelimit(N, D)
 s = time()
 for k in IKEYS:
     r.hit(k)
